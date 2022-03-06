7ddb93fef96a1cc33af4fccb677d8a5ccd7d9a99
LuluPong <frempl@rpi.edu>
2022-03-06 18:42:13 -0500

    Test Upload via Github Desktop

---
+++ Lab4/AngyShodanApp/frontend/lab4/src/app/http.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  private apiserver = "http://localhost:3000/api";
+  private myIPapi = "http://localhost:3000/myIP";
+  private anyIPapi = "http://localhost:3000/postIP";
+  private linkAPI = "http://localhost:3000/getDNS";
+
+  constructor(private httpClient: HttpClient) { }
+
+  public sendGetRequest() {
+    return this.httpClient.get(this.apiserver);
+  }
+
+  public getMyIP() {
+    return this.httpClient.get(this.myIPapi);
+  }
+
+  public exploreIP(ipValue: string) {
+    console.log(ipValue);
+    return this.httpClient.post(this.anyIPapi, JSON.parse(ipValue));
+  }
+
+  public linkToIP(linkValue: string) {
+    console.log(linkValue);
+    return this.httpClient.post(this.linkAPI, JSON.parse(linkValue));
+  }
+}
