5b46cc9fa749bccc4123d5122523dd74145d9dd3
LuluPong <frempl@rpi.edu>
2022-03-22 12:22:50 -0400

    Update server.js

--- Lab5/AngyShodanApp/backend/server.js
+++ Lab5/AngyShodanApp/backend/server.js
@@ -51,7 +51,6 @@ app.route('/db/:number')
             dbo.collection('test').find({doc_id: doc_num}).toArray(function(err, result) {
                 if (err) throw (err)
                 result.forEach(myfunct)
-                //IS GITHUB WORKING?
                 //THIS FUNCTION WORKS
                 //RESULT IS OUTPUT TO NODE CONSOLE (REQUESTED DOCUMENT ONLY (USING doc_id key))
             })
