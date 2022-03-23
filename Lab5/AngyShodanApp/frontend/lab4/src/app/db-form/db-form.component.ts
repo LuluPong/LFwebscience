605a9db5133f7e45c207700baf8b8090b607bc71
LuluPong <frempl@rpi.edu>
2022-03-22 22:12:54 -0400

    Found Form Values when Submitted

--- Lab5/AngyShodanApp/frontend/lab4/src/app/db-form/db-form.component.ts
+++ Lab5/AngyShodanApp/frontend/lab4/src/app/db-form/db-form.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { FormControl } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -15,6 +13,24 @@ export class DbFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  mdbForm = new FormGroup ({
+    document_number: new FormControl(''),
+    content: new FormControl('')
+  })
+
+  onSubmit(event: SubmitEvent) {
+    if (event?.submitter?.innerHTML == "PUT") {
+      console.log("THIS IS A PUT REQUEST")
+    } else if (event?.submitter?.innerHTML == "POST") {
+      console.log("THIS IS A POST REQUEST")
+    } else if (event?.submitter?.innerHTML == "GET") {
+      console.log("THIS IS A GET REQUEST")
+      console.log(this.mdbForm.get('document_number')?.value)
+    } else if (event?.submitter?.innerHTML == "DELETE") {
+      console.log("THIS IS A DELETE REQUEST")
+    }
   }
 
 }
